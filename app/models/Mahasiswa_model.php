534c813f087171c24a0af188dd4ee706f121da59
Arif Hidayat <arief21.hidayat21@gmail.com>
2019-07-25 22:36:54 +0700

    #Database WRAPERRRRRRRRRRRRVNEJMJKRAREJVMLARNJJVLAKVRJENAVR

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,25 +1,21 @@
 <?php
 
 class Mahasiswa_model{
-    //databasehandler
-    private $dbh;
-    //statement untuk query
-    private $stmt;
+    private $tabel = "mahasiswa";
+    private $db;
 
     public function __construct(){
-        //data source name
-        $dsn = "mysql:host=127.0.0.1;dbname=phpmvc";
-
-        try {
-            $this->dbh = new PDO($dsn, "root", "");
-        } catch(PDOExeption $e){
-            die($e->getMessage());
-        }
+        $this->db = new Database();
     }
 
     public function getAllMahasiswa(){
-        $this->stmt = $this->dbh->prepare("SELECT * FROM mahasiswa");
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->tabel);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id){
+        $this->db->query("SELECT * FROM {$this->tabel} WHERE id=:id");
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
